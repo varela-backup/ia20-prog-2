952b4b0e3d166868b18cd6e8cb1484ee117d3ed6
Varela <caldodecode@gmail.com>
2022-03-22 11:09:58 -0300

    ...

--- 002 -  Genius Revisão HTML+CSS+JS - IA Braba/main.js
+++ 002 -  Genius Revisão HTML+CSS+JS - IA Braba/main.js
@@ -1,10 +1,28 @@
 const divMain = document.querySelector("main")
 // const divs = document.querySelectorAll("main div")
 const divs = divMain.querySelectorAll("div")
+const sequencia = []
 
-const rnd = Math.round(Math.random() * 3)
-divs[rnd].classList.add("animate")
-
-divMain.addEventListener("click", (ev) => { 
-    ev.target.classList.toggle("animate") 
+divMain.addEventListener("click", ev => {
+    ev.target.classList.toggle("animate")
 })
+
+// for (let i = 0; i <= 3; i++) {
+//     divs[i].addEventListener("animationend", ev => divs[i].classList.remove("animate"))
+// }
+
+divs.forEach(div => {
+    div.addEventListener("animationend", () => {
+        div.classList.remove("animate")
+    })
+})
+
+function turno() {
+    sequencia.forEach(num => {
+        divs[num].classList.add("animate")
+        // PARAMOS AQUI    
+    })
+    const rnd = Math.round(Math.random() * 3)
+    divs[rnd].classList.add("animate")
+    sequencia.push(rnd)
+}
