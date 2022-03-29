7e42e38e3d9040f58420239885686d5f82a40137
Varela <caldodecode@gmail.com>
2022-03-29 11:07:24 -0300

    playAnimationColors

--- 002 -  Genius Revisão HTML+CSS+JS - IA Braba/main.js
+++ 002 -  Genius Revisão HTML+CSS+JS - IA Braba/main.js
@@ -1,14 +1,21 @@
 const divMain = document.querySelector("main")
-const divs = divMain.querySelectorAll("div")
+const divs = Array.from(divMain.querySelectorAll("div"))
 const sequencia = []
 
 let animatingColors = false
+let currentColorPosition = 0
 
 divMain.addEventListener("click", ev => {
     if (animatingColors) {
         console.log("espere a animação terminar")
         return
     }
+    const idxClickedElement = divs.indexOf(ev.target)
+    if (idxClickedElement !== sequencia[currentColorPosition]) {
+        alert("perdeu playboy!")
+        return
+    }
+    currentColorPosition++
     ev.target.classList.add("animate")
 })
 
@@ -22,11 +29,13 @@ function playAnimationColors() {
     sequencia.forEach((current, index) => {
         setTimeout(() => {
             divs[current].classList.add("animate");
+            animatingColors = index < sequencia.length - 1
         }, 1000 * index);
     })
 }
 
 function turno() {
+    currentColorPosition = 0
     const rnd = Math.round(Math.random() * 3)
     sequencia.push(rnd)
     playAnimationColors()
