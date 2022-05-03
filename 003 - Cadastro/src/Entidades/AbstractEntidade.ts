176cc61a71e5cb2a07737ad6550fcd005fc1b972
Varela <caldodecode@gmail.com>
2022-05-03 08:18:46 -0300

    ...

--- 003 - Cadastro/src/Entidades/AbstractEntidade.ts
+++ 003 - Cadastro/src/Entidades/AbstractEntidade.ts
@@ -1,9 +1,11 @@
 import * as fs from 'node:fs'
 
-type TObjetoGenerico = { [key: string]: any }
+export interface IObjetoGenerico { 
+    [key: string]: any 
+}
 
 export abstract class Entidade {
-    protected _dados: TObjetoGenerico = {}
+    protected _dados: IObjetoGenerico = {}
 
     constructor() { }
     
--- 003 - Cadastro/src/index.ts
+++ 003 - Cadastro/src/index.ts
@@ -1,15 +1,16 @@
 //@ts-ignore
 import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
-import { Pessoa } from "./Entidades/Pessoa"
+import { Aluno } from './Entidades/Aluno' // adicionar
 
 const rl = readline.createInterface({ input, output })
 
 async function main() {
     const nome = await rl.question("qual é o seu nome? ")
     const dataNasc = await rl.question("qual é a sua data de nascimento? ")
-    const pessoa = new Pessoa(nome, new Date(dataNasc))
-    pessoa.store()
+    const matricula = await rl.question("qual é o seu número de matrícula? ")
+    const aluno = new Aluno({ nome, matricula, dataNasc: new Date(dataNasc) })
+    aluno.store()
     process.exit()
 }
 
