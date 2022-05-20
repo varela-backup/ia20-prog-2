f9fe55abd01531cc0d344df1144037f251e54b68
Varela <caldodecode@gmail.com>
2022-05-20 15:48:55 -0300

    ...

--- 003 - Cadastro - Frontend/main.js
+++ 003 - Cadastro - Frontend/main.js
@@ -1,28 +1,38 @@
-const maskElements = document.querySelectorAll("[data-mascara]")
+// const maskElements = document.querySelectorAll("[data-mascara]")
 
-const fnMasks = {
-    matricula: maskMatricula
-}
+// const fnMasks = {
+//     matricula: maskMatricula
+// }
 
-function maskMatricula(el) {
-    el.addEventListener("keydown", ev => { 
-        const key = ev.key
-        if (key == "a") {
-            ev.preventDefault()
-        }
-    })
-}
+// function maskMatricula(el) {
+//     el.addEventListener("keydown", ev => { 
+//         const key = ev.key
+//         if (key == "a") {
+//             ev.preventDefault()
+//         }
+//     })
+// }
 
-maskElements.forEach(el => {
-    const maskName = el.dataset.mascara
-    const fnMascara = fnMasks[maskName]
-    fnMascara(el)
+// maskElements.forEach(el => {
+//     const maskName = el.dataset.mascara
+//     const fnMascara = fnMasks[maskName]
+//     fnMascara(el)
+// })
+
+const actionBar = document.querySelector("div.action-bar")
+const btAdd = actionBar.querySelector(".bt-add")
+const container = document.querySelector(".container-data")
+const templateModalAluno = container.querySelector("template.aluno")
+
+btAdd.addEventListener("click", () => {
+    const cloneModal = templateModalAluno.content.cloneNode(true)
+    container.prepend(cloneModal)
 })
 
-const x = document.querySelector(".modal")
-const main = document.querySelector("main")
-let i = 100;
-while(i--) {
-    const z = x.cloneNode(true)
-    main.appendChild(z)
-}
+container.addEventListener("click", ev => {
+    const btClose = ev.target.closest(".bt-close")
+    if (btClose) {
+        const modal = ev.target.closest(".modal")
+        modal.remove()
+    }
+})
