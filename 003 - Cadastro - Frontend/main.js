28f13753cb9526daeb4d2bb4a43d9da0eac97b65
Varela <caldodecode@gmail.com>
2022-06-14 11:33:28 -0300

    ...

--- 003 - Cadastro - Frontend/main.js
+++ 003 - Cadastro - Frontend/main.js
@@ -26,13 +26,57 @@ const templateModalAluno = container.querySelector("template.aluno")
 
 btAdd.addEventListener("click", () => {
     const cloneModal = templateModalAluno.content.cloneNode(true)
+
+    // CLONAR E ADICIONO A FUNCIONALIDADE (altenativa de abordagem)
+    // const btSave = cloneModal.querySelector(".bt-save")
+    // btSave.addEventListener("click", ev => alert("asdasd"))
+
     container.prepend(cloneModal)
 })
 
-container.addEventListener("click", ev => {
-    const btClose = ev.target.closest(".bt-close")
-    if (btClose) {
+container.addEventListener("click", async ev => {
+    let button
+
+    //
+    //
+    //
+    if (ev.target.closest(".bt-close")) {
         const modal = ev.target.closest(".modal")
         modal.remove()
+        return
     }
+
+    //
+    //
+    //
+    if (button = ev.target.closest(".bt-save")) {
+        const form = ev.target.closest(".modal").querySelector("form")
+        console.log(button, form)
+
+        form.addEventListener("submit", async ev => {
+            ev.preventDefault()
+            button.setAttribute("disabled", true)
+            const req = await fetch("http://127.0.0.1:4000/aluno", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    nome: form.nome.value,
+                    dataNasc: form.dataNasc.value,
+                    matricula: form.matricula.value,
+                })
+            })
+
+            if (req.status == 200) {
+                alert("salvamos seus dados no servidor!")
+                button.removeAttribute("disabled")
+                return
+            }
+
+            button.innerHTML = "ERRO!"
+            alert("Erro ao salvar seus dados no servidor!")
+        })
+
+        return
+    }
+
 })
